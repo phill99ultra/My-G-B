6da4128715e5f089126661c9493651768b060786
Phill <filipcrijanovschi@gmail.com>
2021-04-04 23:18:24 +0300

    watch html files, browsersync

---
+++ gulpfile.js
@@ -0,0 +1,85 @@
+const project_folder = 'build',
+    source_folder  = 'src',
+    path           = {
+        build: {
+            html  : `${project_folder}/`,
+            css   : `${project_folder}/css/`,
+            js    : `${project_folder}/js/`,
+            img   : `${project_folder}/images/`,
+            fonts : `${project_folder}/fonts/`,
+        },
+        src  : {
+            html  : `${source_folder}/*.html`,
+            css   : `${source_folder}/stylus/style.styl`,
+            js    : `${source_folder}/js/main.js`,
+            img   : `${source_folder}/images/**/*.{jpg,png,svg,ico,gif,wepb}`,
+            fonts : `${source_folder}/fonts/*.{ttf,woff}`,
+        },
+        watch: {
+            html  : `${source_folder}/**/*.html`,
+            css   : `${source_folder}/stylus/**/*.styl`,
+            js    : `${source_folder}/js/**/*.js`,
+            img   : `${source_folder}/images/**/*.{jpg,png,svg,ico,gif,wepb}`            
+        },
+        clean: `./${project_folder}/`
+    },
+    { src, dest, parallel } = require('gulp'),
+    gulp                    = require('gulp'),
+    plugin                  = require('gulp-load-plugins')(),
+    browsersync             = require('browser-sync').create(),
+    build                   = gulp.series(html),
+    watch                   = parallel(build, watchFiles, browserSync);
+
+function browserSync() {
+    browsersync.init({
+        server : {
+            baseDir: `./${project_folder}/`
+        },
+        port   : 3344,
+        notify : false
+    });
+}
+
+function html() {
+    return src(path.src.html)
+        .pipe(dest(path.build.html))
+        .pipe(browsersync.stream());
+}
+
+function watchFiles() {    
+    gulp.watch([path.watch.html], html)
+}
+
+exports.html    = html;
+exports.build   = build;
+exports.watch   = watch; 
+exports.default = watch;
+
+// const { src, dest, watch, parallel } = require('gulp'),   
+//     rupture         = require('rupture'),
+//     plugin          = require('gulp-load-plugins')(),
+//     browserSync     = require('browser-sync').create();
+   
+
+
+// function styles() {
+//     return src('./src/stylus/*.styl')
+//         .pipe(plugin.stylus({
+//             use: rupture(),
+//             compress: true
+//         }))
+//         .pipe(plugin.rename('style.min.css'))
+//         .pipe(dest('./build/css'))
+//         .pipe(browserSync.stream())
+// };
+
+// function watching() {
+//     watch(['./src/stylus/**/*.styl'], styles);
+//     watch(['./build/*.html']).on('change', browserSync.reload)
+// }
+
+// exports.styles      = styles;
+// exports.watching    = watching;
+// exports.browsersync = browsersync;
+
+// exports.default     = parallel(watching, browsersync);
